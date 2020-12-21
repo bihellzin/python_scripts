822614528a5a0404fec88d78e44ba0e833516f0a
bihellzin <gof2@cin.ufpe.br>
2020-12-21 18:14:51 -0300

    move_file function added

--- download_organizer.py
+++ download_organizer.py
@@ -2,23 +2,35 @@
 
 import os
 
-from os import listdir
-from os.path import isfile, join
+from os import listdir, replace
+from os.path import isfile, join, splitext
 from pathlib import Path
 
 def get_directories(directory_path: str) -> dict:
     dirs = {}
     for item in listdir(directory_path):
-        if not isfile(join(download_path, f)):
+        if not isfile(join(download_path, item)):
             dirs[item] = True
 
     return dirs
+
+
+def move_file(file_name: str, current_dir_path: str, new_dir: str):
+    current_full_path = current_dir_path + '/' + file_name
+    new_full_path = current_dir_path + '/' + new_dir + '/' + file_name
+
+    replace(current_full_path, new_full_path)
+
 
 if __name__ == "__main__":
     home = str(Path.home())
     os.chdir('{0}/Downloads'.format(home))
     download_path = os.getcwd()
 
+    dirs = get_directories(download_path)
+
     for f in listdir(download_path):
         if isfile(join(download_path, f)):
-            pass
+            _, extension = splitext(f)
+
+            

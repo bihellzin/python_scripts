94871c60067c03ea8e1815c4b058f0920d03337a
bielzin <oliva.gabriel@outlook.com>
2021-01-15 18:14:23 -0300

    beatiful print

--- temperature.py
+++ temperature.py
@@ -5,8 +5,10 @@ from subprocess import check_output
 cpu_output = check_output(['sensors']).decode("utf-8").split("\n")[3:9]
 gpu_output = check_output(['nvidia-smi']).decode("utf-8").split("\n")[9]
 
-
+print("+-------------------------------------------------------------+")
 for i in cpu_output:
-  print(i)
+  print("|  {i}  |".format(i=i))
+print("|=============================================================|")
+print("|  GPU:           +{}.0°C  (high = +90.0°C, crit = +120.0°C)  |".format(gpu_output[8:10]))
 
-print("GPU:           +{}.0°C  (high = +90.0°C, crit = +120.0°C)".format(gpu_output[8:10]))
+print("+-------------------------------------------------------------+")

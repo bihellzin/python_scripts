8f5f53665f4d32ab1edad44ca09f9b3de1d26040
bielzin <oliva.gabriel@outlook.com>
2021-01-21 22:24:56 -0300

    fixing indentation

--- temperature.py
+++ temperature.py
@@ -3,25 +3,25 @@
 from subprocess import check_output
 
 def get_output_list(command: str, *args) -> list:
-  command_line = []
-  command_line.append(command)
+    command_line = []
+    command_line.append(command)
 
-  for arg in args:
-    command_line.append(arg)
+    for arg in args:
+        command_line.append(arg)
 
-  return check_output(command_line).decode("utf-8").split("\n")
+    return check_output(command_line).decode("utf-8").split("\n")
 
 
 if __name__ == "__main__":
-  cpu_output = get_output_list("sensors")[3:9]
-  gpu_output = get_output_list("nvidia-smi")[9]
-  nvme_output = get_output_list("sudo", "nvme", "smart-log", "/dev/nvme0n1")[2]
+    cpu_output = get_output_list("sensors")[3:9]
+    gpu_output = get_output_list("nvidia-smi")[9]
+    nvme_output = get_output_list("sudo", "nvme", "smart-log", "/dev/nvme0n1")[2]
 
-  print("+-------------------------------------------------------------+")
-  for i in cpu_output:
-    print("|  {i}  |".format(i=i))
-  print("|=============================================================|")
-  print("|  GPU:           +{}.0°C  (high = +90.0°C, crit = +120.0°C)  |".format(gpu_output[8:10]))
-  print("|  NVMe:          +{}.0°C  (high = +70.0°C, crit = + 70.0°C)   |".format(nvme_output[38:40]))
+    print("+-------------------------------------------------------------+")
+    for i in cpu_output:
+        print("|  {i}  |".format(i=i))
+    print("|=============================================================|")
+    print("|  GPU:           +{}.0°C  (high = +90.0°C, crit = +120.0°C)  |".format(gpu_output[8:10]))
+    print("|  NVMe:          +{}.0°C  (high = +70.0°C, crit = + 70.0°C)   |".format(nvme_output[38:40]))
 
-  print("+-------------------------------------------------------------+")
+    print("+-------------------------------------------------------------+")
